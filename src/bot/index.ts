6840e4422f5acfa1f4778830e88bd1631fb3d116
jeffery-prod <241513000+jeffery-prod@users.noreply.github.com>
2026-02-20 20:07:26 -0500

    bot-setup: Initial bot setup in Vercel structure -- testing

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,24 +1,20 @@
-  import { Bot, webhookCallback } from 'grammy';
-  import * as dotenv from 'dotenv';
+import { Bot } from 'grammy';
+import * as dotenv from 'dotenv';
 
-  dotenv.config();
+dotenv.config();
 
-  export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
+export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
 
-  bot.use(async (ctx, next) => {
-    if (ctx.from?.id !== Number(process.env.TELEGRAM_USER_ID)) {
-        await ctx.reply('You are not my daddy.')
-        return;
-    }
-    await next();
-  });
-
-  bot.command('start', (ctx) => ctx.reply('start: testing the command'));
-
-  const isProduction = process.env.NODE_ENV === 'prod';
-
-  if (!isProduction) {
-    bot.start();
+bot.use(async (ctx, next) => {
+  if (ctx.from?.id !== Number(process.env.TELEGRAM_USER_ID)) {
+    await ctx.reply('You are not my daddy.');
+    return;
   }
+  await next();
+});
 
-  export default isProduction ? webhookCallback(bot, 'http') : null;
+bot.command('start', (ctx) => ctx.reply('start: testing the command'));
+
+if (process.env.NODE_ENV !== 'production') {
+  bot.start();
+}
